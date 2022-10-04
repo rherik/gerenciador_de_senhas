5a2b61eb2c2946cc0090a5fd0c7955e37c3d48c1
hcunha.ciee <herikrosa@hotmail.com>
2022-10-04 12:48:06 -0300

    Add novas funcionalidades

--- app.py
+++ app.py
@@ -2,40 +2,27 @@ import os
 from keys import *
 from interface import *
 from time import sleep
+import getpass
 """
+Se não houver um arquivo de senhas neste diretório o app cria um arquivo .json sem nome. 
+
 Tarefas:
 - Armazenar a variável opcao em uma função no arquivo interface
-- Informar quando o arquivo estiver vazio
-- Estudar uma possível implementação do kivy ou fazer interface gráfica com pyqt5
-- Implementar um sistema de backup
-- Listar arquivos 
+- Informar quando o arquivo estiver vazio e listar arquivos
+- Fazer interface gráfica com pyqt5
+- Implementar um sistema de backup 
+- Melhorar a troca de arquivos
 """
 
 
 def app():
-    '''
-    Tarefas:
-    - Melhorar a troca de arquivos
-    '''
-    #introducao()
-    lista_dos_arquivos = lista_de_arquivos()
-    if len(lista_dos_arquivos) == 0:
-        print("Você não possui arquivo de armazenamento de senhas.")
-        arq_usuario = input(
-                "Insira o nome do seu arquivo de senhas: ")
-    elif len(lista_dos_arquivos) == 1:
-        # Consertar: Nome fica com .json duas vezes
-        for n in lista_dos_arquivos:
-            arquivo_da_funcao = n[-5]
-        print(f"Seu arquivo de senhas será:\n {arquivo_da_funcao}")
-        arq_usuario = arquivo_da_funcao
-    else:
-        listar_arquivos(lista_dos_arquivos)
-        arq_usuario = input(
-                "Insira o nome do seu arquivo de senhas: ")
-    
+
+    introducao()
+
+    arquivo_usuario = encontra_arquivo()
+
     while True:
-        if arq_usuario == "0":
+        if arquivo_usuario == "0":
             sleep(1)
             print("Encerrando...")
             sleep(1)
@@ -43,25 +30,34 @@ def app():
 
         usuario = Gerenciador()
         # usuario.nome = arq_usuario
-  
-        arquivo_usuario = arq_usuario + ".json"
+
+        arquivo_usuario = verifica_json(arquivo_usuario)
 
         if not os.path.isfile(arquivo_usuario):
             dict_inicial = {}
             with open(arquivo_usuario, "w") as arquivo:
                 json.dump(dict_inicial, arquivo)
-                
+
         sleep(1)
         opcao = input("\nO que você quer fazer? "
-                      "\n- Digite 0 para encerrar o app"
-                      "\n- Digite 1 para gerar uma nova senha"
-                      "\n- Digite 2 para incluir uma senha existente"
-                      "\n- Digite 3 para listar todas as senhas"
-                      "\n- Digite 4 para criar ou alterar de usuário"
-                      "\n- Digite 5 para listar os arquivos de senhas"
-                      "\n- Digite 6 para exluir um login"
-                      "\n- Digite 7 para excluir todo o conteúdo do arquivo \n")
-                
+                      "\n- Digite 0 para encerrar o app;\n"
+
+                      "\n- Digite 1 para gerar uma nova senha;\n"
+
+                      "\n- Digite 2 para incluir uma senha existente;\n"
+
+                      "\n- Digite 3 para listar todas as senhas;\n"
+
+                      "\n- Digite 4 para criar ou alterar de usuário;\n"
+
+                      "\n- Digite 5 para listar os arquivos de senhas;\n"
+
+                      "\n- Digite 6 para exluir um login;\n"
+
+                      "\n- Digite 7 para excluir todo o conteúdo do arquivo; \n"
+
+                      "\n- Digite 8 para excluir um arquivo. \n")
+
         if opcao == "0":
             sleep(1)
             print("Encerrando...")
@@ -91,7 +87,7 @@ def app():
 
         elif opcao == "2":
             login = input("Qual é o novo login? ")
-            senha = input("Qual é a nova senha? ")
+            senha = getpass.getpass("Qual é a nova senha? ")
             nova_senha = usuario.trata_dicio(arquivo_usuario, login, senha)
 
             if usuario.incluir_conteudo(arquivo_usuario, nova_senha):
@@ -101,19 +97,19 @@ def app():
 
         elif opcao == "3":
             usuario.ler_arquivo(arquivo_usuario)
-            
+
         elif opcao == "4":
-            arq_usuario = input(
-            "Insira o nome do seu arquivo de senhas: ")
+            arquivo_usuario = input(
+                "Insira o nome do seu arquivo de senhas: ")
 
         elif opcao == "5":
-            listar_arquivos()
-            
+            listar_arquivos(lista_de_arquivos())
+
         elif opcao == "6":
             login = input("Qual login você quer excluir? ")
-            
-            login_apagado= usuario.apagar_chave(arquivo_usuario, login)
-            
+
+            login_apagado = usuario.apagar_chave(arquivo_usuario, login)
+
             if usuario.incluir_conteudo(arquivo_usuario, login_apagado):
                 print("Login e senha apagados com sucesso.")
             else:
@@ -131,6 +127,18 @@ def app():
                     break
                 else:
                     print("Digite s para Sim e n para Não.")
+
+        elif opcao == "8":
+            arquivo = input("Qual é o nome do arquivo que você quer excluir? ")
+            arquivo = verifica_json(arquivo)
+            if arquivo == arquivo_usuario:
+                arquivo_usuario = ""
+            try:
+                os.remove(arquivo)
+                print("Arquivo excluído com sucesso.")
+            except Exception as e:
+                print(f"Erro {e} ao excluír arquivo. Tente novamente.")
+
         else:
             print("Insira uma opção válida.")
             sleep(1)
--- interface.py
+++ interface.py
@@ -1,6 +1,12 @@
 from time import sleep
 import os
 
+"""
+Tarefas:
+- Informar quando o arquivo estiver vazio e listar arquivos
+- Melhorar a troca de arquivos
+"""
+
 
 def lacos(tempo, vezes):
     sleep(tempo)
@@ -28,5 +34,28 @@ def lista_de_arquivos():
 def listar_arquivos(arquivos_listados):
     print("Os arquivos são: ")
     for n in arquivos_listados:
-        print(n)
+        print(f"{n}\n")
+        sleep(1)
         
+        
+def encontra_arquivo():
+    lista_dos_arquivos = lista_de_arquivos()
+    if len(lista_dos_arquivos) == 0:
+        print("Você não possui arquivo de armazenamento de senhas.")
+        arquivo = input(
+                "Insira o nome do seu arquivo de senhas: ")
+    elif len(lista_dos_arquivos) == 1:
+        for n in lista_dos_arquivos:
+            arquivo_da_funcao = n
+        print(f"Seu arquivo de senhas atual é:\n {arquivo_da_funcao}")
+        arquivo = arquivo_da_funcao
+    else:
+        listar_arquivos(lista_dos_arquivos)
+        arquivo = input(
+                "Insira o nome do seu arquivo de senhas: ")
+    return arquivo
+
+def verifica_json(arquivo_usuario):
+    if ".json" not in arquivo_usuario:
+        arquivo_usuario = arquivo_usuario + ".json"
+    return arquivo_usuario
